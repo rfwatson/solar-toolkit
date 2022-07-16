b3b6f28401c7e475df7c01ac5b14f07246a14d3b
Rob Watson <rob@netflux.io>
2022-07-16 22:39:26 +0200

    modbus: Validate response

--- command/modbus.go
+++ command/modbus.go
@@ -1,5 +1,28 @@
 package command
 
+//go:generate stringer -type=FailureCode
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+)
+
+type FailureCode byte
+
+const (
+	FailureCodeIllegalFunction FailureCode = iota + 1
+	FailureCodeIllegalDataAddress
+	FailureCodeIllegalDataValue
+	FailureCodeSlaveDeviceFailure
+	FailureCodeAcknowledge
+	FailureCodeSlaveDeviceBusy
+	FailureCodeNegativeAcknowledgement
+	FailureCodeMemoryParityError
+	FailureCodeGatewayPathUnavailable
+	FailureCodeGatewayTargetDeviceFailedToRespond
+)
+
 var modbusCrcTable []uint16
 
 func init() {
@@ -19,7 +42,10 @@ func init() {
 }
 
 type ModbusCommand struct {
-	payload []byte
+	payload     []byte
+	commandType ModbusCommandType
+	offset      uint16
+	value       uint16
 }
 
 const modbusComAddr byte = 0xf7
@@ -27,7 +53,8 @@ const modbusComAddr byte = 0xf7
 type ModbusCommandType byte
 
 const (
-	ModbusCommandTypeRead       ModbusCommandType = 0x03
+	ModbusCommandTypeRead ModbusCommandType = 0x03
+	// TODO: implement write commands.
 	ModbusCommandTypeWrite      ModbusCommandType = 0x06
 	ModbusCommandTypeWriteMulti ModbusCommandType = 0x10
 )
@@ -45,7 +72,12 @@ func NewModbus(commandType ModbusCommandType, offset uint16, value uint16) *Modb
 	p = append(p, byte(sum&0xff))
 	p = append(p, byte((sum>>8)&0xff))
 
-	return &ModbusCommand{payload: p}
+	return &ModbusCommand{
+		payload:     p,
+		commandType: commandType,
+		offset:      offset,
+		value:       value,
+	}
 }
 
 func modbusChecksum(b []byte) uint16 {
@@ -58,6 +90,42 @@ func modbusChecksum(b []byte) uint16 {
 
 func (cmd ModbusCommand) String() string { return string(cmd.payload) }
 
+// ValidateResponse validates the entire response and if valid returns the
+// response body.
 func (cmd ModbusCommand) ValidateResponse(p []byte) ([]byte, error) {
-	return p[5 : len(p)-2], nil
+	if len(p) < 4 {
+		return nil, errors.New("invalid response: response too short")
+	}
+
+	var expectedLen int
+	cmdType := ModbusCommandType(p[3])
+
+	switch cmdType {
+	case ModbusCommandTypeRead:
+		if uint16(p[4]) != cmd.value*2 {
+			return nil, fmt.Errorf("short response: expected %d, got %d", p[4], cmd.value*2)
+		}
+		expectedLen = int(p[4]) + 7
+		if len(p) < expectedLen {
+			return nil, fmt.Errorf("invalid read length: expected %d, got %d", expectedLen, len(p))
+		}
+	case ModbusCommandTypeWrite, ModbusCommandTypeWriteMulti:
+		panic("unimplemented")
+	default:
+		expectedLen = len(p)
+	}
+
+	offset := expectedLen - 2
+	wantSum := modbusChecksum(p[2:offset])
+	gotSum := binary.LittleEndian.Uint16(p[offset:])
+	if wantSum != gotSum {
+		return nil, fmt.Errorf("invalid CRC-16: want `%X`, got `%X`", wantSum, gotSum)
+	}
+
+	if p[3] != byte(cmd.commandType) {
+		failureCode := FailureCode(p[4])
+		return nil, fmt.Errorf("command failed with code: %d, error: %s", failureCode, failureCode.String())
+	}
+
+	return p[5:offset], nil
 }
